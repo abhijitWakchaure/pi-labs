ad18bec72826bac0a38b6df93071397ef18eba36
abhijit wakchaure <abhijitwakchaure.2014@gmail.com>
2022-07-03 20:01:58 +0530

    make changes for base path

--- RGB_Cooling_HAT/install.sh
+++ RGB_Cooling_HAT/install.sh
@@ -5,5 +5,5 @@ sudo echo 'hdmi_force_hotplug=1' >> /boot/config.txt
 cd /home/pi/.config/
 mkdir /home/pi/.config/autostart
 cd /home/pi/.config/autostart
-cp /home/pi/RGB_Cooling_HAT/start.desktop /home/pi/.config/autostart/
+cp /home/pi/pi-labs/RGB_Cooling_HAT/RGB_Cooling_HAT.desktop /home/pi/.config/autostart/
 echo 'install ok!'
